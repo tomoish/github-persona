a58698606f1f90e794aabbc16fb39207b6554c87
kou7306 <kouta.1206.y@icloud.com>
2024-02-01 20:31:34 +0900

    get-rank

---
+++ src/funcs/get_activity.go
@@ -0,0 +1,124 @@
+package funcs
+
+import (
+	"bytes"
+	"fmt"
+	"encoding/json"
+	"net/http"
+	"github.com/tomoish/readme/funcs"
+)
+
+const (
+	// GitHubのアクセストークン
+	accessToken = "YOUR_GITHUB_ACCESS_TOKEN"
+	// GitHubのGraphQL APIエンドポイント
+	apiEndpoint = "https://api.github.com/graphql"
+)
+// / GraphQLQuery はGraphQLのクエリを格納するための構造体です
+type GraphQLQuery struct {
+	Query string `json:"query"`
+}
+
+// GitHubのGraphQLクエリを定義
+type response struct {
+	Data struct {
+		User struct {
+			ContributionsCollection struct {
+				TotalCommitContributions        int `graphql:"totalCommitContributions"`
+				TotalIssueContributions         int `graphql:"totalIssueContributions"`
+				TotalPullRequestContributions   int `graphql:"totalPullRequestContributions"`
+				TotalRepositoryContributions    int `graphql:"totalRepositoryContributions"`
+			} `graphql:"contributionsCollection"`
+			StarredRepositories struct {
+				TotalCount int `graphql:"totalCount"`
+			} `graphql:"starredRepositories"`
+		} `graphql:"user(login: $username)"`
+	}
+}
+
+
+func fetchDataInTimeRange(token string, username string) (int, int, int, int, int, error) {
+	const query_frame = `
+	{
+		user(login: "%s") {
+		  contributionsCollection {
+			totalCommitContributions
+			totalIssueContributions
+			totalPullRequestContributions
+			totalRepositoryContributions
+		  }
+		  starredRepositories {
+			totalCount
+		  }
+		}
+	  }
+	`
+
+	// GraphQLクエリを構築
+	query := fmt.Sprintf(query_frame, username)
+
+	// GraphQLクエリを実行して詳細情報を取得
+	request := GraphQLQuery{Query: query}
+	requestBody, err := json.Marshal(request)
+	if err != nil {
+		return 0, 0, 0, 0, 0, err
+	}
+
+	url := "https://api.github.com/graphql"
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return 0, 0, 0, 0, 0, err
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, 0, 0, 0, 0, err
+	}
+	defer resp.Body.Close()
+
+	var response response
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return 0, 0, 0, 0, 0, err
+	}
+
+	// 総コントリビューション数
+	totalCommitContributions := response.Data.User.ContributionsCollection.TotalCommitContributions
+	// 総スター数
+	totalStarredRepositories := response.Data.User.StarredRepositories.TotalCount
+	// 総Issue数
+	totalIssueContributions := response.Data.User.ContributionsCollection.TotalIssueContributions
+	// 総PullRequest数
+	totalPullRequestContributions := response.Data.User.ContributionsCollection.TotalPullRequestContributions
+	// 総コミット数
+	totalRepositoryContributions := response.Data.User.ContributionsCollection.TotalRepositoryContributions
+
+	return totalCommitContributions, totalStarredRepositories, totalIssueContributions, totalPullRequestContributions, totalRepositoryContributions, nil
+}
+
+
+// func main() {
+// 	// ユーザー名
+// 	username := "kou7306"
+
+// 	// 期間を指定
+// 	// from := time.Date(2024, 2, 1, 0, 0, 0, 0, time.UTC)
+// 	// to := time.Date(2024, 2, 1, 23, 59, 59, 0, time.UTC)
+
+// 	// GitHubのアクセストークンを設定
+// 	token,_ :=funcs.GetTokens(0)
+
+// 	// データを取得
+// 	totalCommitContributions, totalStarredRepositories, totalIssueContributions, totalPullRequestContributions, totalRepositoryContributions, err := fetchDataInTimeRange(token, username)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	fmt.Println("totalCommitContributions: ", totalCommitContributions)
+// 	fmt.Println("totalStarredRepositories: ", totalStarredRepositories)
+// 	fmt.Println("totalIssueContributions: ", totalIssueContributions)
+// 	fmt.Println("totalPullRequestContributions: ", totalPullRequestContributions)
+// 	fmt.Println("totalRepositoryContributions: ", totalRepositoryContributions)
+// }
--- src/go.mod
+++ src/go.mod
@@ -13,5 +13,7 @@ require (
 require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/shurcooL/githubv4 v0.0.0-20240120211514-18a1ae0e79dc // indirect
+	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466 // indirect
 	golang.org/x/image v0.15.0 // indirect
 )
